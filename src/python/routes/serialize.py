3d24bd6ecdcab1d1e4f0d1cbefd2cf507400ed51
Carlos Henriquez <CGHenriquez@users.noreply.github.com>
2026-06-23 22:51:47 +0200

    Update04V3
    Agregado "field_validator" para ejercicio 4

--- src/python/routes/serialize.py
+++ src/python/routes/serialize.py
@@ -2,7 +2,7 @@
 # CODIGO SEGURO
 import json
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, field_validator
 
 router = APIRouter()
 
